638e9d1c0d171c6194aab53e55defccbeb1f5779
Matt Logan <mlogan5212@gmail.com>
2020-08-28 22:37:19 -0500

    create player completed

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PlayerData from '../../helpers/data/playerData';
 import Player from '../Player/Player';
+import PlayerForm from '../PlayerForm/PlayerForm';
 
 import './Team.scss';
 
@@ -12,6 +13,7 @@ class Team extends React.Component {
 
   state = {
     players: [],
+    formOpen: false,
   }
 
   getPlayers = () => {
@@ -33,13 +35,28 @@ class Team extends React.Component {
       .catch((err) => console.error(err));
   }
 
+  createPlayer = (newPlayer) => {
+    PlayerData.createPlayer(newPlayer)
+      .then(() => {
+        this.getPlayers();
+        this.setState();
+      })
+      .catch((err) => console.error(err));
+  }
+
+  closeForm = () => {
+    this.setState({ formOpen: false });
+  }
+
   render() {
-    const { players } = this.state;
+    const { players, formOpen } = this.state;
     const playerCard = players.map((player) => <Player key={player.id} player={player} deletePlayer={this.deletePlayer}/>);
 
     return (
       <div>
       <h1>Big Orange Ballers</h1>
+      { !formOpen ? <button className="btn btn-secondary mb-2" onClick={() => { this.setState({ formOpen: true }); }}>Create New Player</button> : '' }
+      { formOpen ? <PlayerForm createPlayer={this.createPlayer} closeForm={this.closeForm}/> : '' }
       <div className = "rosterContainer">
       {playerCard}
       </div>
